cd3b4359fa2c4204f748065d7a0d4fea102920c1
Armin Vieweg <Armin.Vieweg@sunzinet.com>
2014-09-05 13:39:57 +0200

    [TASK] Rewrite of phpexcel_service
    Upgraded PHPExcel to version 1.8.0.
    Added PHPExcel library directly in extension. No downloading necessary anymore.
    Added README.rst.
    Rewritten Phpexcel Service for TYPO3.
    TYPO3 6.0 or greater is now required.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,37 +3,31 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "phpexcel_service".
  *
- * Auto generated 24-07-2014 23:23
- *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'PHPExcel Library and Service',
-	'description' => 'Provides PHPExcel library and TYPO3 service to use it. IMPORTANT: Execute the update script after installing, to get PHPExcel! This may take a moment.',
+	'description' => 'Provides PHPExcel library and TYPO3 service to use it. PHPExcel is already provided in this extension.',
 	'category' => 'misc',
-	'version' => '1.7.6',
+	'version' => '1.8.0',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'clearcacheonload' => 0,
 	'author' => 'Armin Ruediger Vieweg',
-	'author_email' => 'info@professorweb.de',
+	'author_email' => 'armin@v.ieweg.de',
 	'author_company' => '',
-	'constraints' =>
-	array (
-		'conflicts' =>
-		array (
+	'constraints' => array(
+		'conflicts' => array(
 			'phpexcel_library' => '0.0.0-0.0.0',
 		),
-		'depends' =>
-		array (
-			'typo3' => '4.5.0-6.2.99',
+		'depends' => array(
+			'typo3' => '6.0.0-6.2.99',
 		),
-		'suggests' =>
-		array (
+		'suggests' => array(
 		),
 	),
 );
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,26 +3,31 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-t3lib_extMgm::addService(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
     $_EXTKEY,
-    'phpexcel' /* sv type */,
-    'tx_phpexcel_service' /* sv key */,
-    array(
-
+    'phpexcel',
+    'tx_phpexcel_service',
+    array (
         'title' => 'PHPExcel for TYPO3',
         'description' => '',
-
         'subtype' => '',
-
         'available' => TRUE,
         'priority' => 50,
         'quality' => 50,
-
         'os' => '',
         'exec' => '',
-
-        'classFile' => t3lib_extMgm::extPath($_EXTKEY).'class.tx_phpexcel_service.php',
-        'className' => 'tx_phpexcel_service',
+        'classFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Service/Phpexcel.php',
+        'className' => 'ArminVieweg\PhpexcelService\Service\Phpexcel',
     )
 );
-?>
+
+
+/** @var \ArminVieweg\PhpexcelService\Service\Phpexcel $phpExcelService */
+$phpExcelService = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstanceService('phpexcel');
+$phpExcel = $phpExcelService->getPHPExcel();
+
+// Your excel magic goes here...
+
+/** @var \PHPExcel_Writer_Excel2007 $excelWriter */
+$excelWriter = $phpExcelService->getInstanceOf('PHPExcel_Writer_Excel2007', $phpExcel);
+$excelWriter->save('...');
